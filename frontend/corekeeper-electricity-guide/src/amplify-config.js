748141f78794245215d0218fcc226d81e363f03f
RenrakDivad <vieclipsekd@gmail.com>
2025-08-31 01:00:39 -0500

    fix frontend

--- frontend/corekeeper-electricity-guide/src/amplify-config.js
+++ frontend/corekeeper-electricity-guide/src/amplify-config.js
@@ -1,4 +1,5 @@
 const awsExports = {
+  "aws_project_region": import.meta.env.VITE_AWS_REGION,
   "aws_appsync_graphqlEndpoint": import.meta.env.VITE_APPSYNC_URL,
   "aws_appsync_region": import.meta.env.VITE_AWS_REGION,
   "aws_appsync_authenticationType": 'API_KEY',
